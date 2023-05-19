8330ed5e9ed6f397340a0a187770da4e0a75d494
Shirley <shirleyzhu@college.harvard.edu>
2023-05-19 13:59:49 -0500

    rename location name label

--- WeatherApp/VIPER/View.swift
+++ WeatherApp/VIPER/View.swift
@@ -19,39 +19,39 @@ protocol AnyView {
 class LocationViewController: UIViewController, AnyView {
     var presenter: AnyPresenter?
     
-    private let label: UILabel = {
-        let label = UILabel()
-        label.textAlignment = .center
-        label.isHidden = false
-        return label
+    private let nameLabel: UILabel = {
+        let nameLabel = UILabel()
+        nameLabel.textAlignment = .center
+        nameLabel.isHidden = false
+        return nameLabel
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.addSubview(label)
-        label.center = view.center
+        view.addSubview(nameLabel)
+        nameLabel.center = view.center
         view.backgroundColor = .systemPink
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        label.frame = CGRect(x: 0, y: 0, width: 200, height: 50)
-        label.center = view.center
+        nameLabel.frame = CGRect(x: 0, y: 0, width: 200, height: 50)
+        nameLabel.center = view.center
     }
     
     func update(with weather: Weather) {
         DispatchQueue.main.async {
             print(weather.location.name)
-            self.label.text = weather.location.name
-            self.label.isHidden = false
+            self.nameLabel.text = weather.location.name
+            self.nameLabel.isHidden = false
         }
     }
     
     func update(with error: String) {
         DispatchQueue.main.async {
-            self.label.text = "error"
-            self.label.backgroundColor = UIColor.white
-            self.label.isHidden = false
+            self.nameLabel.text = "error"
+            self.nameLabel.backgroundColor = UIColor.white
+            self.nameLabel.isHidden = false
         }
     }
     
